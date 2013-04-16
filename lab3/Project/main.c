4b93dd1de6e7cc0586f7d9eabd18a88d9bbd4a98
mzimmere <mzimmere@calpoly.edu>
2013-04-16 10:40:58 -0700

    cleared out a bunch

--- lab3/Project/main.c
+++ lab3/Project/main.c
@@ -13,42 +13,24 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define LED0 0x01	//Definition of LED pins
-#define LED2 0x04
-#define LED4 0x10
-#define LED6 0x40
-
-struct task_args {
-   uint16_t period_ms;	//Struct for period of blinking, and LED character
-   uint8_t led;
-};
-
-void vTask(void *tArgs);//Prototype for vTask
+void vHandlerTask(void *tArgs);
+void vLEDTask(void *tArgs);
 
 int main( void )
-{			//Struct initialization with periods and respective LEDs
-   struct task_args tArgs[4] = {{1000,LED0}, {500,LED2}, {250,LED4}, {125,LED6}};
-
-   DDRB = 0xFF; 	//Sets LED port to output, and off
+{
+   DDRB = 0xFF;
    PORTB = 0xFF;
-			//Task calls for all 4 LEDs
-   xTaskCreate(vTask, (const char *) "1Hz", 100, (void *) &tArgs[0], 1, NULL);
-   xTaskCreate(vTask, (const char *) "2Hz", 100, (void *) &tArgs[1], 1, NULL);
-   xTaskCreate(vTask, (const char *) "4Hz", 100, (void *) &tArgs[2], 1, NULL);
-   xTaskCreate(vTask, (const char *) "8Hz", 100, (void *) &tArgs[3], 1, NULL);
 
-   vTaskStartScheduler();//Starts the Tasks running
+   xTaskCreate(vTask, (const char *) "1Hz", 100, (void *) &tArgs[0], 1, NULL);
+
+   // Kick off the scheduler
+   vTaskStartScheduler();
 
    return 0;
 }
 
-void vTask(void *tArgs)	//Task Definition for vTask
+void vTask(void *tArgs)
 {
-			//Struct for using tArgs in vTask
-   struct task_args *args = (struct task_args *) tArgs; 
-			
    for (;;)  {
-      PORTB ^= args->led;//Toggle LED, wait the given period
-      vTaskDelay( (args->period_ms / 2) / portTICK_RATE_MS );
    }
 }
